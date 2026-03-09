5b77975b151bc373a7ef6f9f41b7fb51fdde0933
Dutech-empire <usmanabdulhamidsaleh7@gmail.com>
2026-03-09 18:08:54 +0100

    Fix missing reserveMBController import

--- src/routes/mb.routes.js
+++ src/routes/mb.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { earnMBController } from "../controllers/mb.controller.js";
-import { borrowMBController } from "../controllers/mb.controller.js";
-
+import {
+  earnMBController,
+  borrowMBController,
+  reserveMBController,
+  releaseMBController
+} from "../controllers/mb.controller.js";
 const router = express.Router();
 
 router.post("/earn", authenticate, earnMBController);
